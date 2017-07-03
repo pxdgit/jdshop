c63c2a7f4f29a9d38ced182d88b5184457d2fc82
px <123@qq.com>
2017-07-03 13:26:44 +0800

    超链接

--- frontend/widgets/CategoryWidget.php
+++ frontend/widgets/CategoryWidget.php
@@ -13,13 +13,12 @@ class CategoryWidget extends Widget
         $redis=new \Redis();
         $redis->connect('127.0.0.1');
         $category_html=$redis->get('category_html');
-        $redis->del('category_html');
         if($category_html==null){
             echo '数据库';
             $categories=GoodsCategory::findAll(['parent_id'=>0]);
             $category_html=$this->renderFile('@app/widgets/view/category.php',['firstcates'=>$categories]);
             $redis->set('category_html',$category_html,3600);//每七天更新一次分类列表
         }
-//        return $category_html;
+        return $category_html;
     }
 }
