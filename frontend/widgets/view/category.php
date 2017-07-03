fb5ad2147f8f380b0aed0e2cb35a9ec9459befff
px <123@qq.com>
2017-07-03 13:35:54 +0800

    超链接

--- frontend/widgets/view/category.php
+++ frontend/widgets/view/category.php
@@ -1,16 +1,16 @@
 <?php foreach ($firstcates as $firstcate):?>
     <div class="cat item1">
-        <h3><a href="<?php echo Yii::getAlias('@web')?>.'/list/index?id='.<?=$firstcate->id?>"><?=$firstcate->name?></a> <b></b></h3>
+        <h3><a href="<?php echo Yii::getAlias('@web')?>/list/index?id=<?=$firstcate->id?>"><?=$firstcate->name?></a> <b></b></h3>
         <div class="cat_detail">
             <?php foreach ($firstcate->children as $secondcate):?>
                 <dl class="dl_1st">
                     <dt>
-                        <a href="<?php echo Yii::getAlias('@web')?>.'/list/index?id='.<?=$secondcate->id?>"><?=$secondcate->name?></a>
+                        <a href="<?php echo Yii::getAlias('@web')?>/list/index?id=<?=$secondcate->id?>"><?=$secondcate->name?></a>
 
                     </dt>
                     <?php foreach ($secondcate->children as $thirdcate):?>
                         <dd>
-                            <a href="<?php echo Yii::getAlias('@web')?>.'/list/index?id='.<?=$thirdcate->id?>"><?=$thirdcate->name?></a>
+                            <a href="<?php echo Yii::getAlias('@web')?>/list/index?id=<?=$thirdcate->id?>"><?=$thirdcate->name?></a>
                         </dd>
                     <?php endforeach;?>
                 </dl>
