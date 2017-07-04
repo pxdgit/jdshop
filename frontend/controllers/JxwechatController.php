6f9ff8e0c317d580e8f0464f31dbd3830edca0e9
px <123@qq.com>
2017-07-04 15:05:15 +0800

    解除绑定

--- frontend/controllers/JxwechatController.php
+++ frontend/controllers/JxwechatController.php
@@ -12,6 +12,7 @@ namespace frontend\controllers;
 use backend\models\Goods;
 use backend\models\GoodsIntro;
 use frontend\models\Member;
+use frontend\models\Order;
 use yii\web\Controller;
 use EasyWeChat\Foundation\Application;
 use EasyWeChat\Message\News;
@@ -25,6 +26,41 @@ class JxwechatController extends Controller
         $server = $app->server;
         $server->setMessageHandler(function ($message) {
             switch ($message->MsgType){
+                case 'text':
+                    switch ($message->Content){
+                        case '帮助':
+                            return "发送 优惠、解除绑定 等信息 ，有惊喜哟";
+                            break;
+                        case '优惠':
+                            $allgoods=Goods::find()->limit(5)->all();
+                            foreach ($allgoods as $goods){
+                                $news[]= new News([
+                                    'title'       => $goods->name,
+                                    'description' =>GoodsIntro::findOne(['goods_id'=>$goods->id])->content,
+                                    'url'         => 'http://jx.penneyx.cn/goods/index?id='.$goods->id,
+                                    'image'       => 'http://admin.penneyx.cn/'.$goods->logo,
+                                ]);
+                            }
+                            return $news;
+                            break;
+                        case '解除绑定':
+                            $openid=\Yii::$app->session->get('openid');
+                            return $openid.'111111';
+                            if($openid==null || Member::findOne(['openid'=>$openid])==null){
+                                return '您未绑定账户，若需绑定，请前往 http://jx.penneyx.cn/jxwechat/login';
+                            }else{
+                                $member=Member::findOne(['openid'=>$openid,'id'=>\Yii::$app->user->id]);
+                                $member->openid=null;
+                                if($member->save(false)){
+                                    return '解绑账户';
+                                }
+                            }
+
+                            break;
+                    }
+//            * 用户发送【帮助】，回复以下信息“您可以发送 优惠、解除绑定 等信息”
+//            * 用户发送【优惠】，效果和点击【促销商品】相同
+//            * 用户发送【解除绑定】，如用户已绑定账户，则解绑当前openid，并回复解绑成功；否则回复请先绑定账户及绑定页面地址
                 case 'event':
                     if($message->Event=='CLICK'){
                         if($message->EventKey=='promotiongoods'){
@@ -40,6 +76,7 @@ class JxwechatController extends Controller
                                 return $news;
                         }
                     }
+                    break;
             }
         });
         $response = $server->serve();
@@ -124,6 +161,9 @@ class JxwechatController extends Controller
           \Yii::$app->session->set('redirect',\Yii::$app->controller->action->uniqueId);
           return $this->redirect(['jxwechat/login']);
         }
+        $member=Member::findOne(['id'=>\Yii::$app->user->id,'openid'=>$openid]);
+        $allorder=Order::find()->where(['member_id'=>$member->id])->all();
+        var_dump($allorder);
     }
     public function actionAddress(){
         $openid=\Yii::$app->session->get('openid');
@@ -138,6 +178,24 @@ class JxwechatController extends Controller
             \Yii::$app->session->set('redirect',\Yii::$app->controller->action->uniqueId);
             return $this->redirect(['jxwechat/login']);
         }
+        if(\Yii::$app->request->isPost){
+            $request=\Yii::$app->request;
+            if($request->post('newpassword')==$request->post('repassword')){
+                $member=Member::findOne(['openid'=>$openid,'id'=>\Yii::$app->user->id]);
+                if(\Yii::$app->security->validatePassword($request->post('password'),$member->password_hash)){
+                     $member->password_hash=\Yii::$app->security->generatePasswordHash($request->post('newpassword'));
+                     if($member->save(false)){
+                         echo '修改成功';
+                     }
+                }else{
+                    echo '原密码错误';
+                }
+            }else{
+                echo '两次密码不一致';
+            }
+
+        }
+        return $this->renderPartial('pwd');
     }
     public function actionCallback(){
         $app = new Application(\Yii::$app->params['wechat']);
